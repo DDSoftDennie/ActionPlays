99042c0b5bbb910db97d43f6ec77d1530f2de523
Dennie Declercq <dennie@ddsoft.be>
2023-10-20 17:31:41 +0200

    Added basic conf

---
+++ ActionPlays/Model/Conference.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionPlays.Model
+{
+	public class Conference
+	{
+		public DateOnly From { get; set; }
+
+		public DateOnly To { get; set; }
+
+		public string Name { get; set; }
+
+		public Conference(DateOnly from, DateOnly to, string name) { 
+			From = from;
+			To = to;
+			Name = name;
+		}
+
+	
+	}
+}
--- ActionPlays/Program.cs
+++ ActionPlays/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using ActionPlays.Model;
+
+Console.WriteLine("Hello, World of Conferences!");
+Conference c = new Conference(
+	   new DateOnly(2023, 11, 8),
+	new DateOnly(2023, 11, 11),
+	"DevConf");
+Console.WriteLine($"Dennie's next conference is:  {c.Name}, from {c.From} to {c.To}");
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
