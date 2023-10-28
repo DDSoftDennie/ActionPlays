99b5528ffabf7caad93d27d294284ab44090848d
Dennie Declercq <dennie@ddsoft.be>
2023-10-28 22:41:09 +0200

    Added unittesting

---
+++ ActionPlays.Tests/Factories/LocalConferenceFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using ActionPlays.Factories;
+using ActionPlays.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ActionPlays.Tests.Factories
+{
+    public class LocalConferenceFactoryTests
+    {
+        [Fact]
+        public void CreateConferenceTest()
+        {
+            // Arrange
+            LocalConferenceFactory factory = new LocalConferenceFactory();
+            DateOnly from = new DateOnly(2023, 11, 8);
+            DateOnly to = new DateOnly(2023, 11, 11);
+            string name = "DevConf";
+
+            // Act
+            Conference c = factory.CreateConference(from, to, name);
+
+            // Assert
+            Assert.Equal(from, c.From);
+            Assert.Equal(to, c.To);
+            Assert.Equal(name, c.Name);
+        }
+    }
+}
---
+++ ActionPlays.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace ActionPlays.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
